a87544b55d2515fef902142a98c160fc2c179a31
SOCIAL WEB SOLUTION <saadeaala89@gmail.com>
2025-02-15 23:04:18 +0530

    Added Toxicity Checker
    SOCIAL MEDIA RANK : Added Toxicity Checker

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+from modules.toxic_checker import check_toxicity # Detoxify फाइल इम्पोर्ट करें
+
+app = Flask(_name_)
+
+@app.route("/")
+def index():
+return render_template("index.html")
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+text = request.form["text"] # HTML फॉर्म से टेक्स्ट लो
+result = check_toxicity(text) # Detoxify से एनालिसिस करो
+
+# रिजल्ट को HTML पेज में भेजो
+return render_template("index.html", text=text, result=result)
+
+if _name_ == "_main_":
+app.run(debug=True)
---
+++ modules/toxic_checker.py
@@ -0,0 +1,6 @@
+# toxic_checker.py
+from detoxify import Detoxify
+
+def check_toxicity(text):
+result = Detoxify('original').predict(text)
+return result
